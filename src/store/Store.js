a3a31f837c3177c4b23cef111781d9aacd9b5076
Sujata Gunale <tidbitsjs@gmail.com>
2020-09-28 21:40:16 +0530

    redux devTool

--- src/store/Store.js
+++ src/store/Store.js
@@ -9,6 +9,10 @@ const initialState = {
   activeSession: "SECONDS",
 };
 
-const store = createStore(reducer, initialState);
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
